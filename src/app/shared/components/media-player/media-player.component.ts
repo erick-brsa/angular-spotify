ba53261224e2cff6f42219c1f9abc588675642d0
Erick Briones <erickalan518@gmail.com>
2022-07-14 22:28:00 -0500

    Creando los componentes side-bar y media-player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaPlayerComponent implements OnInit {
 
+  mockCover: any = {
+    cover: 'https://lastfm.freetls.fastly.net/i/u/ar0/20157062c639a350bb99d58047b892e5',
+    name: 'Enemy X JID',
+    album: 'Enemy'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-side-bar',
-  templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+	selector: "app-side-bar",
+	templateUrl: "./side-bar.component.html",
+	styleUrls: ["./side-bar.component.css"]
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+	mainMenu: {
+		defaultOptions: Array<any>
+		accessLink: Array<any>
+	} = {
+		defaultOptions: [],
+		accessLink: []
+	}
 
-  ngOnInit(): void {
-  }
+  customOptions: Array<any> = []
 
+	constructor() {}
+
+	ngOnInit(): void {
+		this.mainMenu.defaultOptions = [
+			{
+				name: "Home",
+				icon: "uil uil-estate",
+				router: ["/", "auth"]
+			},
+			{
+				name: "Buscar",
+				icon: "uil uil-search",
+				router: ["/", "history"]
+			},
+			{
+				name: "Tu biblioteca",
+				icon: "uil uil-chart",
+				router: ["/", "favorites"],
+				query: { hola: "mundo" }
+			}
+		]
+
+		this.mainMenu.accessLink = [
+			{
+				name: "Crear lista",
+				icon: "uil-plus-square"
+			},
+			{
+				name: "Canciones que te gustan",
+				icon: "uil-heart-medical"
+			}
+		]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+	}
 }
